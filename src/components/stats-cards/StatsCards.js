6655e897ff015bde19e1b8ec3bbaf0e912504a43
Maxwell Lasky <mlasky46@gmail.com>
2019-10-17 14:01:58 -0600

    CSS on landing page cleanup

--- src/components/stats-cards/StatsCards.js
+++ src/components/stats-cards/StatsCards.js
@@ -4,17 +4,38 @@ import { isEmpty } from 'lodash-es'
 import Card from '../card/Card'
 import withStatsData from '../../hoc/withStatsData'
 
+import './StatsCards.scss'
+
 const StatsCards = ({ stats }) => {
   if (!isEmpty(stats)) {
     const { addresses, contracts, height, transactions } = stats
     const formatNumber = num => num.toLocaleString()
     return (
-      <div id="statistics-card-container">
-        <Card value={formatNumber(addresses)} detail="Total Addresses" />
-        <Card value={formatNumber(height)} detail="Blocks Produced" />
-        <Card value={formatNumber(transactions)} detail="Total Transactions" />
-        <Card value={formatNumber(contracts)} detail="Contracts Deployed" />
-      </div>
+      <React.Fragment>
+        <div id="statistics-card-container" className="stats-as-row">
+          <Card value={formatNumber(addresses)} detail="Total Addresses" />
+          <Card value={formatNumber(height)} detail="Blocks Produced" />
+          <Card
+            value={formatNumber(transactions)}
+            detail="Total Transactions"
+          />
+          <Card value={formatNumber(contracts)} detail="Total Contracts" />
+        </div>
+
+        <div id="statistics-card-container" className="stats-as-columns">
+          <div class="column">
+            <Card value={formatNumber(addresses)} detail="Total Addresses" />
+            <Card value={formatNumber(height)} detail="Blocks Produced" />
+          </div>
+          <div class="column">
+            <Card
+              value={formatNumber(transactions)}
+              detail="Total Transactions"
+            />
+            <Card value={formatNumber(contracts)} detail="Total Contracts" />
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
   return (
