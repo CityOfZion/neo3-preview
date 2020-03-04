1c51df94f0626f3a0230420484fd192880d87768
Zach <zsiglin@gmail.com>
2020-03-04 07:22:40 -0800

    Fix Issue 68 (#69)
    * Fix rerendering issue
    
    * Fix theme flashing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React, { Fragment, useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React, { Fragment, useEffect, useMemo } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from 'react-router-dom'
 
 import Footer from './components/footer/Footer'
 import Navigation from './components/navigation'
@@ -12,96 +17,92 @@ import Contract from './containers/contract/Contract'
 import Transaction from './containers/transaction/Transaction'
 import Address from './containers/address/Address'
 import GettingStarted from './containers/getting-started/GettingStarted'
-import './App.css'
-import withThemeData from './hoc/withThemeData'
 import { applyTheme, DARK_THEME } from './components/toggle/ThemeToggle'
-import usePrevious from './hooks/userPrevious'
+import withThemeData from './hoc/withThemeData'
+import './App.css'
 
-// Causes the router to scroll to the top of the page
-// on any route change but not theme changes
-const ScrollToTop = ({ theme, ...otherProps }) => {
-  const previousProps = usePrevious({ theme }) || {}
-  useEffect(() => {
-    if (previousProps.theme !== theme) {
-      return
-    }
-    return window.scrollTo(0, 0)
-  }, [otherProps, theme, previousProps.theme])
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+  useEffect(() => window.scrollTo(0, 0), [pathname])
 
   return null
 }
 
 const App = ({ theme, setTheme }) => {
   const { mode } = theme
+  const themeIsDefined = Boolean(mode)
 
   useEffect(() => {
     const savedTheme = localStorage.getItem('neo3-preview-theme')
     applyTheme(savedTheme || DARK_THEME, setTheme)
   }, [setTheme])
 
-  return mode ? (
-    <Fragment>
-      <div className="content">
-        <Router>
-          <Navigation />
-          <div className="router-content">
-            <Route
-              component={props => <ScrollToTop {...props} theme={theme} />}
-            />
-            <Switch>
-              <Route
-                path="/transactions"
-                component={props => <Transactions {...props} />}
-              />
-              <Route
-                path="/transaction/:id"
-                component={props => <Transaction {...props} />}
-              />
-              <Route
-                path="/block/:id"
-                component={props => <Block {...props} />}
-              />
-              <Route
-                path="/blocks/:page"
-                component={props => <Blocks {...props} />}
-                exact
-              />
-              <Route
-                path="/blocks"
-                component={props => <Blocks {...props} />}
-                exact
-              />
-              <Route
-                path="/contract/:id"
-                component={props => <Contract {...props} />}
-              />
-              <Route
-                path="/contracts/:page"
-                component={props => <Contracts {...props} />}
-                exact
-              />
-              <Route
-                path="/contracts"
-                component={props => <Contracts {...props} />}
-                exact
-              />
-              <Route
-                path="/address/:id"
-                component={props => <Address {...props} />}
-              />
-              <Route
-                path="/getting-started"
-                component={props => <GettingStarted {...props} />}
-              />
-              <Route path="/" component={props => <LandingPage {...props} />} />
-            </Switch>
+  return useMemo(
+    () =>
+      themeIsDefined && (
+        <Fragment>
+          <div className="content">
+            <Router>
+              <Navigation />
+              <ScrollToTop />
+              <div className="router-content">
+                <Switch>
+                  <Route
+                    path="/transactions"
+                    component={props => <Transactions {...props} />}
+                  />
+                  <Route
+                    path="/transaction/:id"
+                    component={props => <Transaction {...props} />}
+                  />
+                  <Route
+                    path="/block/:id"
+                    component={props => <Block {...props} />}
+                  />
+                  <Route
+                    path="/blocks/:page"
+                    component={props => <Blocks {...props} />}
+                    exact
+                  />
+                  <Route
+                    path="/blocks"
+                    component={props => <Blocks {...props} />}
+                    exact
+                  />
+                  <Route
+                    path="/contract/:id"
+                    component={props => <Contract {...props} />}
+                  />
+                  <Route
+                    path="/contracts/:page"
+                    component={props => <Contracts {...props} />}
+                    exact
+                  />
+                  <Route
+                    path="/contracts"
+                    component={props => <Contracts {...props} />}
+                    exact
+                  />
+                  <Route
+                    path="/address/:id"
+                    component={props => <Address {...props} />}
+                  />
+                  <Route
+                    path="/getting-started"
+                    component={props => <GettingStarted {...props} />}
+                  />
+                  <Route
+                    path="/"
+                    component={props => <LandingPage {...props} />}
+                  />
+                </Switch>
+              </div>
+            </Router>
           </div>
-        </Router>
-      </div>
-      <Footer />
-    </Fragment>
-  ) : (
-    <div />
+          <Footer />
+        </Fragment>
+      ),
+    [themeIsDefined],
   )
 }
 
